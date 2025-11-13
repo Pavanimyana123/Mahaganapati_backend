f785b7db716231ca93766b0a15b1dd54024a15ff
pavanimyana123 <pavanimyana2000@gmail.com>
2025-11-13 14:31:43 +0530

    Pavani

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -3,15 +3,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'vangalatharun2001@gmail.com',
-    pass: 'zcja yyad cvgx bsyk'
+    user: 'palankar1234@gmail.com',
+    pass: 'uowb ymzc flyq vwhh'
   },
   tls: {
     rejectUnauthorized: false
   }
 });
 
-const ADMIN_EMAIL = 'vangalatharun2001@gmail.com';
+const ADMIN_EMAIL = 'palankar1234@gmail.com';
 
 module.exports = {
   transporter,
